64f4e546c878d7e194102ed2e701a0b0f53d023f
HimanshuMotiani <himanshu.motiani93@gmail.com>
2021-09-28 17:12:33 +0530

    response-object-change

--- block-BNaaap/index.js
+++ block-BNaaap/index.js
@@ -1,8 +1,9 @@
 var http = require("http")
 var server = http.createServer(handleRequest)
 function handleRequest(req,res){
+    console.log(req.method)
     res.statusCode = 201;
-    res.setHeader(`Content-Type`,`text/plain`)
-    res.end("Welcome");
+    res.setHeader(`content-Type`,`text/html`)
+    res.end("<h1>Welcome to dharamshala</h1>");
 }
 server.listen(4444,()=>{})
