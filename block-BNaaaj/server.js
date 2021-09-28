dd6f935528f86d1235cdf9b1f49e4f1414f51638
HimanshuMotiani <himanshu.motiani93@gmail.com>
2021-09-28 11:53:26 +0530

    url-changes

--- block-BNaaaj/server.js
+++ block-BNaaaj/server.js
@@ -1,7 +1,6 @@
 let url = require("url")
-let parsedUrl = url.parse("https://airindia.com/fares/calculate?from=delhi&to=detroit")
+let parsedUrl = url.parse("https://airindia.com/fares/calculate?from=delhi&to=detroit",true)
 console.log(parsedUrl)
 console.log(parsedUrl.query)
 console.log(parsedUrl.protocol)
 console.log(parsedUrl.pathname);
-console.log(url.parse(parsedUrl.query))
